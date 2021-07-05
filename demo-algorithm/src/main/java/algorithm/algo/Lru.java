532158cab59cdb2ee0ba462776e02a0502a53604
zyl <840017241@qq.com>
2021-07-05 19:52:11 +0800

    lru

---
+++ demo-algorithm/src/main/java/algorithm/algo/Lru.java
@@ -0,0 +1,30 @@
+package algorithm.algo;
+
+/**
+ * @author Eric 840017241@qq.com
+ * @date 2021/6/29 23:12
+ * <p>
+ * desc: LRU最近最少使用淘汰算法
+ * 双向链表实现
+ */
+public class Lru {
+
+//    In short, the microservice architectural style [1] is an approach to developing a
+//    single application as a suite of small services, each running in its own process and
+//    communicating with lightweight mechanisms, often an HTTP resource API. These services
+//    are built around business capabilities and independently deployable by fully automated
+//    deployment machinery. There is a bare minimum of centralized management of these
+//    services, which may be written in different programming languages and use different
+//    data storage technologies.
+
+
+
+
+//    Starters are a set of convenient dependency descriptors that you can include in your
+//    application. You get a one-stop shop for all the Spring and related technologies that
+//    you need without having to hunt through sample code and copy-paste loads of dependency
+//    descriptors. For example, if you want to get started using Spring and JPA for database
+//    access, include the spring-boot-starter-data-jpa dependency in your project.
+
+
+}
