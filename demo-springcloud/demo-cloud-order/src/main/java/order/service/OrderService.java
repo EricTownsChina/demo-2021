811171cde989e5c5e01fc78e0cc15c4e86872a9b
EricTownsChina <840017241@qq.com>
2021-07-10 15:29:31 +0800

    spring cloud nacos config

--- demo-springcloud/demo-cloud-order/src/main/java/order/service/OrderService.java
+++ demo-springcloud/demo-cloud-order/src/main/java/order/service/OrderService.java
@@ -1,15 +1,10 @@
 package order.service;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 /**
  * @author Eric 840017241@qq.com
@@ -18,18 +13,25 @@ import java.util.concurrent.Future;
  * Desc: order 订单服务对外暴露的接口
  * 服务注册到注册中心上
  */
+@RefreshScope
 @RestController
 public class OrderService {
 
     @Value("${server.port}")
     private String port;
 
+    @Value("${user.name}")
+    private String userName;
+
+    @Value("${user.age}")
+    private Integer userAge;
+
     @GetMapping("/orders")
     public String all() {
 
         System.out.println("port: " + port);
 
-        return "this is all orders";
+        return "this is all orders, user name = " + userName + ", user age = " + userAge;
     }
 
 }
